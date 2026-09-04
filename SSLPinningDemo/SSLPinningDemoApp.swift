f902ce81a153fb848aebd71f4874b7cdc43be315
Sujeet Kumar <sujeetkumartiwarry@gmail.com>
2026-09-04 23:38:25 +0530

    Initial Commit

---
+++ SSLPinningDemo/SSLPinningDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSLPinningDemoApp.swift
+//  SSLPinningDemo
+//
+//  Created by Sujeet kumar on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SSLPinningDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
